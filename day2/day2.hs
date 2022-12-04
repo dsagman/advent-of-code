f2e16992af330382de1c90a6c8f56e00048ebf3a
dsagman <70487040+dsagman@users.noreply.github.com>
2022-12-04 10:37:12 -0600

    day2

--- day2/day2.hs
+++ day2/day2.hs
@@ -3,28 +3,46 @@ module Main where
 main :: IO ()
 main = do
         nums <- readFile "day2/input.txt"
-        let games = [[head x, (strategy . last) x] | x <- lines nums ]
-        let myScores = zipWith (+) (map evalScore games) (map evalBonus games)
+        let games1 = [[head x, (strategy1 . last) x] | x <- lines nums ]
+        let myScores1 = zipWith (+) (map evalScore games1) (map evalBonus games1)
 
         putStr "Part 1 answer: " 
-        print $ sum myScores
+        print $ sum myScores1
 
-        -- putStr "Part 2 answer: " 
-        -- print $ (sum . take 3 . reverse . sort) food
+        let games2 = [[head x, strategy2 x (last x)] | x <- lines nums ]
+        let myScores2 = zipWith (+) (map evalScore games2) (map evalBonus games2)
+
+        putStr "Part 2 answer: " 
+        print $ sum myScores2
 
 evalScore :: Num a => String -> a
 evalScore x
     | x `elem` wins     = 6
     | head x == last x  = 3
     | otherwise         = 0
-    where wins = ["AB", "BC", "CA"]
 
-strategy :: Char -> Char
-strategy x =
-    case x of
+wins :: [String]
+wins = ["AB", "BC", "CA"]
+
+strategy1 :: Char -> Char
+strategy1 s =
+    case s of
         'X' -> 'A'
         'Y' -> 'B'
         'Z' -> 'C'
+
+-- X means you need to lose, 
+-- Y means you need to end the round in a draw, 
+-- and Z means you need to win.
+
+strategy2 :: [Char] -> Char -> Char
+strategy2 g s =
+    case s of
+        'X' -> (head . head . filter (\x -> f == last x)) wins
+        'Y' -> f
+        'Z' -> (last . head . filter (\x -> f == head x)) wins
+        where f = head g
+
 
 evalBonus :: Num a => [Char] -> a
 evalBonus x = 
--- day2/day2.py
+++ day2/day2.py
@@ -1,25 +1,38 @@
 datafile = open("day2/input.txt", "r")
 
 moves = [(x[0],x[2]) for x in datafile]
-print(moves)
-
-strategy = {'X': 'A', 'Y': 'B', 'Z': 'C'}
-
 wins = [('A', 'B'), ('B', 'C'), ('C', 'A')]
-scores = {'A': 1, 'B': 2, 'C': 3}
-myScore = 0
+bonus = {'A': 1, 'B': 2, 'C': 3}
+
+strategy1 = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+myScore1 = 0
 for move in moves:
-    decoded = (move[0],strategy[move[1]])
-    myScore += scores[decoded[1]]
+    decoded = (move[0],strategy1[move[1]])
+    myScore1 += bonus[decoded[1]]
     if decoded in wins:
-        print("win")
-        myScore += 6
+        # print("win")
+        myScore1 += 6
     elif decoded[0] == decoded[1]:
-        print("draw")
-        myScore += 3
-    else:
-        print("lose")
+        # print("draw")
+        myScore1 += 3
 
+def strategy2(f, s):
+    if s == 'X': return losesDict[f]
+    if s == 'Y': return f
+    if s == 'Z': return winsDict[f]
 
-print("Part 1 answer: ", myScore)
-# print("Part 2 answer: ", sum(sums[0:3]))
+winsDict = dict(wins)
+losesDict = {v:k for k,v in winsDict.items()} 
+myScore2 = 0
+for move in moves:
+    decoded = (move[0],strategy2(move[0],move[1]))
+    myScore2 += bonus[decoded[1]]
+    if decoded in wins:
+        # print("win")
+        myScore2 += 6
+    elif decoded[0] == decoded[1]:
+        # print("draw")
+        myScore2 += 3
+
+print("Part 1 answer: ", myScore1)
+print("Part 2 answer: ", myScore2)
