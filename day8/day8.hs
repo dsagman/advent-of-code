965bb3c814ff6ee0b3dba477e8f2618230566b58
dsagman <70487040+dsagman@users.noreply.github.com>
2022-12-09 10:50:03 -0600

    day 8 part 2

--- day8/day8.hs
+++ day8/day8.hs
@@ -1,7 +1,6 @@
 module Main where
  
-import Data.List 
-import Data.List.Split 
+import Data.List ( inits, transpose ) 
 
 
 main :: IO ()
@@ -13,60 +12,69 @@ main = do
         let fromLeft  = map lookLst trees
         let fromRight = map lookLst $ map reverse trees
         let fromTop   = map lookLst $ transpose trees
-        -- let fromBot   = map lookLst $ (reverse . transpose) trees
         let fromBot   = map lookLst $ map reverse $ transpose trees
-
-        -- mapM_ print fromLeft
-        mapM_ print trees
-        print ""
-        mapM_ print (map reverse trees)
 
         let gridLeft  = fromLeft 
         let gridRight = map reverse fromRight
         let gridTop   = transpose fromTop 
         let gridBot   = (reverse . transpose) fromBot 
 
-        print "from left"
-        mapM_ print $ gridLeft
-        print "from right"
-        mapM_ print $ gridRight
-        print "from top"
-        mapM_ print $ gridTop
-        print "from bottom"
-        mapM_ print $ gridBot
+        let combined = foldl1 add2dLst [gridLeft, gridRight, gridTop, gridBot]
+        let answer1 = length $ concatMap (filter (>0)) combined
 
-        let grids = [gridLeft, gridRight, gridTop, gridBot]
-        let combined = foldl1 add2dLst grids
-        print "combined"
-        mapM_ print $ combined
+        let fromLeft2  = map lookLst2 trees
+        let fromRight2 = map lookLst2 $ map reverse trees
+        let fromTop2   = map lookLst2 $ transpose trees
+        let fromBot2   = map lookLst2 $ map reverse $ transpose trees
 
-        print $ length $ concatMap (filter (>0)) combined
+        let gridLeft2  = fromLeft2 
+        let gridRight2 = map reverse fromRight2
+        let gridTop2   = transpose fromTop2 
+        let gridBot2   = (reverse . transpose) fromBot2 
 
-    
-        let answer1 =  "no"
-        let answer2 = "dice"
+        let combined2 = foldl1 mul2dLst [gridLeft2, gridRight2, gridTop2, gridBot2]
+
+        let answer2 = (maximum . map maximum) combined2
+
         putStr "Part 1 answer: " 
         print answer1
 
         putStr "Part 2 answer: " 
         print answer2
 
--- 30373
--- 25512
--- 65332
--- 33549
--- 35390
 
+add2dLst :: [[Int]] -> [[Int]] -> [[Int]]
 add2dLst = zipWith (zipWith(+))
 
+mul2dLst :: [[Int]] -> [[Int]] -> [[Int]]
+mul2dLst = zipWith (zipWith(*))
+
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil _ [] = []
+takeUntil p (x:xs) = x : if p x then takeUntil p xs else []
+
+lookLst :: (Num b, Ord a) => [a] -> [b]
 lookLst xs =  (map isTaller . tail . inits) xs
 
+isTaller :: (Num a1, Ord a2) => [a2] -> a1
 isTaller xs 
     | length xs == 1 = 1 
     | otherwise = if aTree > (maximum upTo) then 1 else 0
     where 
           upTo = init xs
           aTree = last xs
+
+lookLst2 :: Ord a => [a] -> [Int]
+lookLst2 xs = (map canSee . tail . inits) xs
+
+canSee :: Ord a => [a] -> Int
+canSee xs 
+    | length xs == 1 = 0
+    | otherwise = length $ takeUntil (<aTree) upTo
+    where 
+          upTo = (reverse . init) xs
+          aTree = last xs
+
 
 makeGrid :: [Int] -> Int -> [[Int]]
 makeGrid xs gSize = [(take n $ repeat 1) ++ (take (gSize - n) $ repeat 0) | n <- xs]
