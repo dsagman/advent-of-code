3227c0cea61e9d219e25c0fe098bf56864f1a02d
dsagman <70487040+dsagman@users.noreply.github.com>
2024-12-17 19:05:53 -0600

    Update day13.hs

--- 2024/day13/day13.hs
+++ 2024/day13/day13.hs
@@ -34,11 +34,22 @@ main = do
     let dataFile = "2024/day13/day.txt"
     input <- readFile dataFile
     let buttons = groupButtons $ filter (/=("X",0,0)) $ map parseLine $ lines input
-    -- mapM_ print buttons
     let part1sols = filter (/= []) $ map findAB buttons
-    -- print $ part1sols
     let part1cost = sum $ (concatMap . map) costAB part1sols
     print $ part1cost
+    -- mapM_ (print . part2prize) buttons
+    let buttons2 = map part2prize buttons
+    let part2sols = map solveAB buttons
+    let part2cost = sum $ map costAB part2sols
+    print $ part2cost
+
+part2prize ::[(String, Int, Int)] -> [(String, Int, Int)]
+part2prize button = [aBut, bBut, (nameP, xP+10000000000000, yP+10000000000000)]
+    where
+        [aBut, bBut, prize] = button
+        (nameA, xA, yA) = aBut
+        (nameB, xB, yB) = bBut
+        (nameP, xP, yP) = prize
 
 costAB :: (Int, Int) -> Int
 costAB (a,b) = 3*a + b
@@ -51,3 +62,13 @@ findAB button = [(a,b) | a <- [0..100], b <- [0..100], a*xA + b*xB == xP, a*yA +
         (nameB, xB, yB) = bBut
         (nameP, xP, yP) = prize
 
+-- b=(py*ax-px*ay)/(by*ax-bx*ay) a=(px-b*bx)/ax        
+solveAB :: [(String, Int, Int)] -> (Int, Int)
+solveAB button = (a,b)
+    where
+        b = (yP*xA - xP*yA) `div` (yB*xA - xB*yA)
+        a = (xP - b*xB) `div` xA
+        [aBut, bBut, prize] = button
+        (nameA, xA, yA) = aBut
+        (nameB, xB, yB) = bBut
+        (nameP, xP, yP) = prize
