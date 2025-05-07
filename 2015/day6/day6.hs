2a27463fca0c11fdc9cbff74f5ee64ce42e15cbe
Silverman, David <70487040+dsagman@users.noreply.github.com>
2025-05-07 17:00:35 -0500

    Update day6.hs

--- 2015/day6/day6.hs
+++ 2015/day6/day6.hs
@@ -14,44 +14,91 @@ import Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
 import Data.List (foldl')
 
-size = 10
+import Text.Parsec
+import Text.Parsec.String (Parser)
+import System.Exit (exitFailure)
 
-toggle :: (Int,Int) -> (Int, Int) -> Map (Int, Int) Int -> Map (Int, Int) Int
-toggle (x1,y1) (x2,y2) grid = 
-    foldl' update grid keys
+type Cmd = (String, XY, XY)
+type XY = (Int, Int)
+type Grid = (Map (Int, Int) Int)
+type Instruction = (String, XY, XY)
+
+cmdParser :: Parser (String, XY, XY)
+cmdParser = do
+    cmd <- try (string "turn on")
+       <|> try (string "turn off")
+       <|> string "toggle"
+    space
+    x1 <- read <$> many1 digit
+    char ','
+    y1 <- read <$> many1 digit
+    string " through "
+    x2 <- read <$> many1 digit
+    char ','
+    y2 <- read <$> many1 digit
+    return (cmd, (x1, y1), (x2, y2))
+
+-- inputParser :: Parser [[Int]]
+inputParser =  do
+    many $ try (cmdParser <* newline)
+
+
+size :: Int
+size = 1000
+
+keys :: XY -> XY -> [XY]
+keys (x1,y1) (x2,y2) = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
+
+toggle :: XY -> XY -> Grid -> Grid
+toggle xy1 xy2 grid =
+    foldl' update grid (keys xy1 xy2)
     where f _ x = Just (1 - x)
-          keys = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
           update g k = Map.updateWithKey f k g
 
-turnOff :: (Int,Int) -> (Int, Int) -> Map (Int, Int) Int -> Map (Int, Int) Int
-turnOff (x1,y1) (x2,y2) grid = 
-    foldl' update grid keys
+turnOff :: XY -> XY -> Grid -> Grid
+turnOff xy1 xy2 grid =
+    foldl' update grid (keys xy1 xy2)
     where
-        keys = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
-        update g k = Map.adjust (const 0) k g        
+        update g k = Map.adjust (const 0) k g
 
-turnOn :: (Int,Int) -> (Int, Int) -> Map (Int, Int) Int -> Map (Int, Int) Int
-turnOn (x1,y1) (x2,y2) grid = 
-    foldl' update grid keys
+turnOn :: XY -> XY -> Grid -> Grid
+turnOn xy1 xy2 grid =
+    foldl' update grid (keys xy1 xy2)
     where
-        keys = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
-        update g k = Map.adjust (const 1) k g    
+        update g k = Map.adjust (const 1) k g
 
-lights :: [((Int, Int), Int)]
+lights :: [(XY, Int)]
 lights = [((x,y),0) | x <- [0..(size-1)], y <-[0..(size-1)]]
 
-lightsMap :: Map (Int, Int) Int
-lightsMap = Map.fromList lights 
+lightsMap :: Grid
+lightsMap = Map.fromList lights
+
+numLit = Map.foldl' (+) 0
+
+evalInst :: Instruction -> Grid -> Grid
+evalInst (inst, xy1, xy2) g = case inst of
+    "turn off" -> turnOff xy1 xy2 g
+    "turn on"  -> turnOn xy1 xy2 g
+    "toggle"   -> toggle xy1 xy2 g
 
 main :: IO ()
 main = do
-    displayMap $ turnOff (3,3) (6,6) $ toggle (0,0) (9,9) lightsMap
+    input <- readFile "./2015/day6/day.txt"
+    dataSet <- case parse inputParser "" input of
+        Left err -> do
+            putStrLn $ "Error parsing input: " ++ show err
+            exitFailure
+        Right x -> pure x
+    let finalLightsMap = foldl' (flip evalInst) lightsMap dataSet
+    print $ numLit lightsMap
+    print "Answer Part 1:"
+    print $ numLit finalLightsMap
 
 
-displayMap :: Map (Int, Int) Int -> IO ()
-displayMap m = mapM_ putStrLn [ row y | y <- [0..(size - 1)] ]
+displayMap :: Grid -> IO ()
+displayMap grid = mapM_ putStrLn [ row y | y <- [0..(size - 1)] ]
   where
-    row y = [ display (Map.findWithDefault 0 (x, y) m) | x <- [0..(size - 1)] ]
+    row y = [ display (Map.findWithDefault 0 (x, y) grid) | x <- [0..(size - 1)] ]
     display 0 = '.'  -- off
     display _ = '#'  -- on
 
